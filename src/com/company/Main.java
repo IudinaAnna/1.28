6bb6a37c1618f2318de0714066b4e0a0d0165746
louisgromova999@gmail.com <onedirection124598>
2021-12-11 16:30:26 +0200

    lab 1 ex 28

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+	float pi=3.14159f;
+    System.out.println("radius: ");
+    int r = input.nextInt();
+    float diametr=2*r;
+    System.out.println("diametr: "+diametr);
+    float circumference=2*pi*r;
+    System.out.println("круг"+circumference);
+    float area =pi*(r*r);
+    System.out.println("p:"+area);
+    }
+}
